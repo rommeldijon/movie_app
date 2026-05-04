dc22e56675aa113972fdef9ef46040b8932ba814
Rommel Dijon <rdijon@ymail.com>
2026-05-04 13:39:03 -0700

    completed Appwrite setup including metrics

--- services/appwrite.ts
+++ services/appwrite.ts
@@ -1,20 +1,41 @@
 // track the searches made by the user
 import '@/interfaces/interfaces.d';
 import { client } from "@/lib/appwrite";
-import { Databases, Query } from "react-native-appwrite";
+import { Databases, ID, Query } from "react-native-appwrite";
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
 
 const database = new Databases(client);
 
 export const updateSearchCount = async (query: string, movie: Movie) => {
+      try {  
       const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID,[
          Query.equal('searchTerm', query)
       ])
 
-      console.log(result)
-    // check if a record of that search has already been stored
-    //if a document is found increment the searcCount field
-    // if no document is found
-       // create a new document in Appwrite database => 1
+      if (result.documents.length > 0) {
+        const existingMovie = result.documents[0];
 
-
-}
+        await database.updateDocument(
+            DATABASE_ID,
+            COLLECTION_ID,
+            existingMovie.$id,
+            {
+                count: existingMovie.count + 1
+            }
+        )
+      } else {
+        await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+            searchTerm: query,
+            movie_id: movie.id,
+            count: 1,
+            title: movie.title,
+            poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        });
+      }
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
